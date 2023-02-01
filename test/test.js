d8f9cacd8754e594c3be142c66cddec13d7757cf
Matt Carter <m@ttcarter.com>
2023-02-02 09:26:31 +1000

    REFACTOR: Moved build worker into its own library

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ import {dirName} from '@momsfriendlydevco/es6';
 import {expect} from 'chai';
 import express from 'express';
 import expressLogger from 'express-log-url';
-import jitMiddleware from '#lib/expressMiddleware';
+import JITMiddleware from '#lib/expressMiddleware';
 
 const __dirname = dirName();
 const port = 8181;
@@ -14,11 +14,11 @@ describe('@MomsFriendlyDevCo/JIT-Build', ()=> {
 	// Server setup {{{
 	let server;
 	before('setup a server', function(finish) {
-		var app = express();
+		let app = express();
 		app.use(expressLogger);
 		app.set('log.indent', '      ');
 
-		app.get('/api/components/:file', jitMiddleware({
+		app.get('/api/components/:file', JITMiddleware({
 			source: req => `${__dirname}/data/${req.params.file}`,
 			dest: req => `/tmp/esbuild.${req.params.file}.js`,
 		}))
