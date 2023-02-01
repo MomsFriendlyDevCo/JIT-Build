6c8bcd2edd6815d1f72d7b7ee1a8c2fdab03a88d
Matt Carter <m@ttcarter.com>
2023-02-01 17:01:18 +1000

    TASK: Initial version

---
+++ test/test.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import {dirName} from '@momsfriendlydevco/es6';
+import {expect} from 'chai';
+import express from 'express';
+import expressLogger from 'express-log-url';
+import jitMiddleware from '#lib/expressMiddleware';
+
+const __dirname = dirName();
+const port = 8181;
+const url = `http://localhost:${port}`;
+
+describe('@MomsFriendlyDevCo/JIT-Build', ()=> {
+
+	// Server setup {{{
+	let server;
+	before('setup a server', function(finish) {
+		var app = express();
+		app.use(expressLogger);
+		app.set('log.indent', '      ');
+
+		app.get('/api/components/:file', jitMiddleware({
+			source: req => `${__dirname}/${req.param.file}`,
+		}))
+
+		// FIXME: Routes
+		server = app.listen(port, null, finish);
+	});
+	after(()=> server && server.close());
+	// }}}
+
+	it('should serve a compiled .vue file', ()=>
+		axios.get(`${url}/api/components/widgets.vue`)
+			.then(({data}) => {
+				expect(data).to.be.a('string');
+				console.log('GOT', data);
+			})
+	)
+
+});
